af4d9e7a7218ca59a59d33116fb9b31aa210cebd
Lily Wang <lily@minium.com.au>
2020-06-26 23:22:02 +1000

    update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -93,7 +93,8 @@ todo_include_todos = True
 
 mathjax_path = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.0/MathJax.js?config=TeX-AMS-MML_HTMLorMML'
 
-site_url = "https://userguide.mdanalysis.org/"
+# site_url = "https://userguide.mdanalysis.org/"
+site_url = "https://mdauserguide.minium.com.au/"
 html_use_opensearch = 'https://userguide.mdanalysis.org'
 
 # Add any paths that contain templates here, relative to this directory.
